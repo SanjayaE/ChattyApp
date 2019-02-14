f208ae53963f938f87acd41d541fbe90e74390de
SanjayaE <sanjaya4u@gmail.com>
2019-02-14 00:08:01 +0000

    implement id and add random id gen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ChatBar from "./ChatBar.jsx";
 import MessageList from "./MessageList.jsx";
+import { generateRandomId } from "./rid.jsx";
 
 export default class App extends Component {
   // Set initial state
@@ -14,22 +15,26 @@ export default class App extends Component {
           type: "incomingMessage",
           content:
             "I won't be impressed with technology until I can download food.",
-          username: "Anonymous1"
+          username: "Anonymous1",
+          id: 1
         },
         {
           type: "incomingNotification",
-          content: "Anonymous1 changed their name to nomnom"
+          content: "Anonymous1 changed their name to nomnom",
+          id: 2
         },
         {
           type: "incomingMessage",
           content:
             "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-          username: "Anonymous2"
+          username: "Anonymous2",
+          id: 3
         },
         {
           type: "incomingMessage",
           content: "...",
-          username: "nomnom"
+          username: "nomnom",
+          id: 4
         }
       ]
     };
@@ -39,8 +44,8 @@ export default class App extends Component {
     let newMsg = {
       type: "incomingMessage",
       content: msg,
-      username: usr
-      //id: generateRandomId()
+      username: usr,
+      id: generateRandomId()
     };
     this.setState({ messages: [...this.state.messages, newMsg] });
   };
